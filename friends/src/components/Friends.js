59c17389d3394b2ff53687acbc7681904367d1bb
Galo Sandoval <galosan92@gmail.com>
2020-08-18 17:20:46 -0700

    added helper functions in friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -9,8 +9,51 @@
 //   })
 // }
 
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const initialFriends = [];
+
+const initialFormValues = {
+  name: "",
+  age: "",
+  email: "",
+};
 
 export const Friends = () => {
-  
-}
+  const [illBeThereForYou, SetIllBeThereForYou] = useState(initialFriends);
+  const [form, setForm] = useState(initialFormValues);
+
+  const addFriend = {
+    ...form
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post('api/friends', addFriend)
+    .then(res => {
+      SetIllBeThereForYou([
+        ...illBeThereForYou, addFriend
+      ])
+    })
+  }
+
+  const onChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("api/friends")
+      .then((res) => {
+        console.log(res);
+        SetIllBeThereForYou(res.data);
+      });
+  }, []);
+
+  return null;
+};
